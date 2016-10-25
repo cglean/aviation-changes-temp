d43c88b287dcd8714e6c97ec8d72827214599dc1
Bansal <amit.bansal@capgemini.com>
2016-10-25 11:22:56 +0530

    Refactored Code Review Changes

--- src/main/java/com/aviation/controller/AviationController.java
+++ src/main/java/com/aviation/controller/AviationController.java
@@ -1,7 +1,5 @@
 package com.aviation.controller;
 
-
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -20,75 +18,49 @@ import com.aviation.entity.Component;
 import com.aviation.entity.Filter;
 import com.aviation.service.AviationService;
 
-
-
-
-
-
 @RestController
 public class AviationController {
-	
-	
+
 	@Autowired
 	private AviationService aviationService;
-	
-	
-	
-	
-	
-	
+
 	@RequestMapping(value = "/loadComponent/{startDate}/{endDate}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Component> loadComponentData(@PathVariable final String startDate,@PathVariable final String endDate)throws ParseException {
-		
+	public List<Component> loadComponentData(@PathVariable final String startDate, @PathVariable final String endDate)
+			throws ParseException {
+
 		String pattern = "YYYY-MM-DD";
-		Date sDate=null;
-		Date eDate=null;
+		Date sDate = null;
+		Date eDate = null;
 		try {
-			sDate =  new SimpleDateFormat(pattern).parse(startDate);
-			 eDate =  new SimpleDateFormat(pattern).parse(endDate);
+			sDate = new SimpleDateFormat(pattern).parse(startDate);
+			eDate = new SimpleDateFormat(pattern).parse(endDate);
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		
-		System.out.println(AviationController.class + "loadComponentData");
-		
 		return aviationService.getComponent(sDate, eDate);
-		
-		
 	}
 
-	
 	@RequestMapping(value = "/saveFilter", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-	public void saveFilter(@RequestBody   Filter filter )throws ParseException {
+	public void saveFilter(@RequestBody Filter filter) throws ParseException {
 		aviationService.saveFilter(filter);
-		
-		System.out.println(AviationController.class + "saveFilter");
-		
 	}
-	
-	
+
 	@RequestMapping(value = "/saveAsDefaultFilter", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-	public void saveAsDefaultFilter(@RequestBody   Filter filter )throws ParseException {
+	public void saveAsDefaultFilter(@RequestBody Filter filter) throws ParseException {
 		aviationService.saveAsDefaultFilter(filter);
-		
-		System.out.println(AviationController.class + "saveAsDefaultFilter");
-		
 	}
-	
-	
+
 	@RequestMapping(value = "/getFilters", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody 
-	public List<Filter> getFilters()  {
-		
-		System.out.println(AviationController.class + "getFilters"); 
+	@ResponseBody
+	public List<Filter> getFilters() {
+
 		return aviationService.getFilters();
-		
+
 	}
-	
+
 	@RequestMapping(value = "/getDefaultFilter", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Filter getDefaultFilter() {
-		System.out.println("data "+aviationService.getDefaultFilter());
-		return aviationService.getDefaultFilter();	
+		return aviationService.getDefaultFilter();
 	}
 
 }
---
+++ src/main/java/com/aviation/util/PathConstants.java
@@ -0,0 +1,15 @@
+package com.aviation.util;
+
+/**
+ * 
+ * @author ambansal
+ *
+ */
+public class PathConstants {
+
+	private PathConstants() {
+		super();
+	}
+
+	
+}
