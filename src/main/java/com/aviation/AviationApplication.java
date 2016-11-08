528e8e890f71c6c4fcac30bde206bcfcb8e5fcac
Bansal <amit.bansal@capgemini.com>
2016-11-08 16:21:26 +0530

    updated

--- src/main/java/com/aviation/AviationApplication.java
+++ src/main/java/com/aviation/AviationApplication.java
@@ -2,9 +2,12 @@ package com.aviation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableWebSecurity
 public class AviationApplication {
 
